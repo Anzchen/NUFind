6ff6827031d2aacacc73f2cb874c494a24a8c95e
Anzchen <Anthonyzchen@yahoo.com>
2022-12-08 14:45:32 -0500

    Routes

--- flask-app/app.py
+++ flask-app/app.py
@@ -15,3 +15,28 @@ if __name__ == '__main__':
     # Take a look at the docker-compose.yml to see 
     # what port this might be mapped to... 
     app.run(debug = True, host = '0.0.0.0', port = 4000)
+
+@app.route("/form")
+def get_form():
+    return """
+    <h2>HTML Forms</h2>
+
+    <form action="/form" method="POST">
+    <label for="first">First name:</label><br>
+    <input type="text" id="first" name="first" value="John"><br>
+    <label for="last">Last name:</label><br>
+    <input type="text" id="last" name="last" value="Doe"><br><br>
+    <input type="submit" value="Submit">
+    </form> 
+    """
+
+@app.route("/form", methods = ['POST'])
+def post_form():
+   first_name = request.form['first']
+   last_name = request.form['last']
+   return f'<h1>Hello {first_name} {last_name}.</h1>'
+
+@app.route("/event", methods = ['POST'])
+def add_event():
+    current_app.logger.info(request.form)
+    return "hello"
