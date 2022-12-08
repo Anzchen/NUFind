552551f12b6945c18fbe8e204e049b586115bf76
Anzchen <Anthonyzchen@yahoo.com>
2022-12-08 13:52:10 -0500

    Update views.py

--- flask-app/src/views.py
+++ flask-app/src/views.py
@@ -13,3 +13,23 @@ def home():
 @views.route('/test')
 def tester():
     return "<h1>this is a test!</h1>"
+
+@app.route("/form")
+def get_form():
+    return """
+    <h2>HTML Forms</h2>
+
+    <form action="/form" method="POST">
+    <label for="first">First name:</label><br>
+    <input type="text" id="first" name="first" value="John"><br>
+    <label for="last">Last name:</label><br>
+    <input type="text" id="last" name="last" value="Doe"><br><br>
+    <input type="submit" value="Submit">
+    </form> 
+    """
+
+@app.route("/form", methods = ['POST'])
+def post_form():
+   first_name = request.form['first']
+   last_name = request.form['last']
+   return f'<h1>Hello {first_name} {last_name}.</h1>'
